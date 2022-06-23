4f56334faab0c21f059cee995086be4d04ff6b32
Altynay18 <altynai.kzh17@gmail.com>
2022-06-24 01:00:10 +0600

    set student status implemented

--- src/components/StudentTable/StudentTable.tsx
+++ src/components/StudentTable/StudentTable.tsx
@@ -14,25 +14,26 @@ import {Authentication} from '../../api/authentication';
 import {User} from '../../api/users';
 
 
-function createData(
-  id: number,
-  firstName: string,
-  lastName: string,
-  progress: string,
-  track: string,
-  email: string,
-  cv: string,
-) {
-  return {id, firstName, lastName, progress, track, email, cv};
-}
+// function createData(
+//   id: number,
+//   firstName: string,
+//   lastName: string,
+//   progress: string,
+//   track: string,
+//   email: string,
+//   cv: string,
+// ) {
+//   return {id, firstName, lastName, progress, track, email, cv};
+// }
 
-const rows = [
-  createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
-  createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
-  createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
-  createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
-  createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
-];
+// const rows = [
+//   createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
+//   createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
+//   createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
+//   createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
+//   createData(1, 'Aizada', 'Turarova', 'stepik', 'frontend', 'aizada.turarova@gmail.com', 'cv'),
+// ];
+
 
 export function DataTable() {
   const [users, setUsers] = useState([]);
@@ -42,59 +43,72 @@ export function DataTable() {
   async function getAllUsers() {
     const data = await screening.getAll();
     console.log("data: ", data);
+    setUsers(data);
   }
 
   async function deleteUser(id) {
     const screeningData = await userData.deleteUser(id);
   }
 
+  const handleChange=(e)=>{
+    console.log(e);
+    userData.changeStatus(e.target.value);
+  }
+  
+
   useEffect(() => {
     getAllUsers();
   }, []);
+
+  console.log("users::::" , users);
+
   return (
     <TableContainer component={Paper}>
+      
       <Table sx={{minWidth: 650}} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell style={{fontWeight: 'bold'}} align="left">id</TableCell>
             <TableCell style={{fontWeight: 'bold'}} align="left">First name</TableCell>
+            <TableCell style={{fontWeight: 'bold'}} align="left">Middle name</TableCell>
             <TableCell style={{fontWeight: 'bold'}} align="left">Last name</TableCell>
-            <TableCell style={{fontWeight: 'bold'}} align="left">Progress</TableCell>
-            <TableCell style={{fontWeight: 'bold'}} align="left">Track</TableCell>
+            <TableCell style={{fontWeight: 'bold'}} align="left">Status</TableCell>
             <TableCell style={{fontWeight: 'bold'}} align="left">Email</TableCell>
             <TableCell style={{fontWeight: 'bold'}} align="left">Cv</TableCell>
             <TableCell align="left"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {users.map((row) => (
             <TableRow
-              // key={r}
               sx={{'&:last-child td, &:last-child th': {border: 0}}}
             >
               <TableCell align="left" component="th" scope="row">
                 {row.id}
               </TableCell>
               <TableCell align="left" component="th" scope="row">
-                {row.firstName}
+              {row.name}
               </TableCell>
-              <TableCell align="left">{row.lastName}</TableCell>
+              <TableCell align="left" component="th" scope="row">
+              {row.middleName}
+              </TableCell>
+              <TableCell align="left">{row.surname}</TableCell>
               <TableCell align="left"><Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 value={''}
                 label="Age"
-                // onChange={}
+                onChange={handleChange}
               >
-                <MenuItem value={10}>0</MenuItem>
-                <MenuItem value={20}>1</MenuItem>
-                <MenuItem value={30}>2</MenuItem>
-                <MenuItem value={30}>3</MenuItem>
+                <MenuItem value={0}>CV</MenuItem>
+                <MenuItem value={1}>Stepik</MenuItem>
+                <MenuItem value={2}>Easyhire</MenuItem>
+                <MenuItem value={3}>Zoom interview</MenuItem>
 
               </Select></TableCell>
-              <TableCell align="left">{row.track}</TableCell>
+              {/* <TableCell align="left">{row.track}</TableCell> */}
               <TableCell align="left">{row.email}</TableCell>
-              <TableCell align="left">{row.cv}</TableCell>
+              {/* <TableCell align="left">{row.cv}</TableCell> */}
               <TableCell align="left"><Button onClick={() => deleteUser(3)} variant="outlined"
                                               color="error">delete</Button></TableCell>
 
